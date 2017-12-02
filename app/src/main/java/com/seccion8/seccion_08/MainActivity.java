3377c99333f4931b647c5b3103d2a7caea6795f7
Kevin ramirez <kevin.ramirez@jbge.com.mx>
2017-12-01 18:12:40 -0600

    Navigation drawer

--- app/src/main/java/com/seccion8/seccion_08/MainActivity.java
+++ app/src/main/java/com/seccion8/seccion_08/MainActivity.java
@@ -1,13 +1,45 @@
 package com.seccion8.seccion_08;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
+    private void setToolbar() {
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        // Habilitar el icono alado de la barra y mostrarlo
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // abrir el menu lateral
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
